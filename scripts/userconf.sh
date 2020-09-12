eedc9add47b73e3633959df652d9417f08135a74
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2020-09-12 11:35:56 +0530

    Minor fix in user

--- scripts/userconf.sh
+++ scripts/userconf.sh
@@ -1,3 +1,3 @@
 apt-get install whois sudo -y
-useradd --create-home --password $(mkpasswd -s $USERPASS) -s $(which bash) $(ADMIN_USER) 
-usermod -aG sudo nava
+useradd --create-home --password $(mkpasswd -s $USERPASS) -s $(which bash) $ADMIN_USER 
+usermod -aG sudo $ADMIN_USER
