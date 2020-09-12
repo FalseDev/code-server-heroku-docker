48295240e7cb097debc259fc57b82dee891e6e7c
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2020-09-12 09:47:05 +0530

    Hotfix

--- install_code_server.sh
+++ install_code_server.sh
@@ -1,23 +1,11 @@
 # Update and upgrade and then install code-server requirements and curl
-apt-get update -y && \
-  apt-get upgrade -y && \
-  apt-get install -y apt-utils && \
-  apt-get install -y && \
-  build-essential \
-  pkg-config \
-  libx11-dev \
-  libxkbfile-dev \
-  libsecret-1-dev \
-  python3 \
-  curl \
-  tmux \
+apt-get update -y && apt-get upgrade -y && \
+  apt-get install -y \
+  build-essential pkg-config gnupg2 libx11-dev libxkbfile-dev libsecret-1-dev \
   ca-certificates \
-  zsh 
-
-echo 'deb https://mega.nz/linux/MEGAsync/xUbuntu_18.04/ ./' > /etc/apt/sources.list.d/mega-nz.list
-
-curl -fsSL https://mega.nz/keys/MEGA_signing.key | apt-key add -
-
-apt-get update && apt-get install megacmd
-
+  python3 curl tmux zsh && \
 curl -fsSL https://code-server.dev/install.sh | sh
+
+echo 'deb https://mega.nz/linux/MEGAsync/xUbuntu_18.04/ ./' > /etc/apt/sources.list.d/mega-nz.list && \
+curl -fsSL https://mega.nz/keys/MEGA_signing.key | apt-key add - && \
+apt-get update && apt-get install megacmd
