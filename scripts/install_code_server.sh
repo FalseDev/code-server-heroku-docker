928a383df71ce608aea6aab85ae4f32b0702c1dd
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2020-09-12 11:33:00 +0530

    Minor fix

--- scripts/install_code_server.sh
+++ scripts/install_code_server.sh
@@ -1,7 +1,7 @@
 # Update and upgrade and then install code-server requirements and curl
 apt-get update -y && apt-get upgrade -y
 
-apt-get install -y build-essential pkg-config gnupg2 libx11-dev libxkbfile-dev libsecret-1-dev python3 
+apt-get install -y build-essential pkg-config gnupg2 libx11-dev libxkbfile-dev libsecret-1-dev python3 curl
 
 # Install script
 curl -fsSL https://code-server.dev/install.sh | sh
