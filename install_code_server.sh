e735a50c6ed0deca149d10db9a020f27cff77365
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2020-09-12 09:48:41 +0530

    Added -y flag

--- install_code_server.sh
+++ install_code_server.sh
@@ -8,4 +8,4 @@ curl -fsSL https://code-server.dev/install.sh | sh
 
 echo 'deb https://mega.nz/linux/MEGAsync/xUbuntu_18.04/ ./' > /etc/apt/sources.list.d/mega-nz.list && \
 curl -fsSL https://mega.nz/keys/MEGA_signing.key | apt-key add - && \
-apt-get update && apt-get install megacmd
+apt-get update && apt-get install megacmd -y
