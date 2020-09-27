f7363c1e5e8e5ab4c835851070b90cbe4b2f014e
Navaneethan <59121948+FalseDev@users.noreply.github.com>
2020-09-27 14:49:08 +0530

    Fix golang extension id

--- scripts/code_extensions.sh
+++ scripts/code_extensions.sh
@@ -4,4 +4,4 @@ install-vs-ext ms-python.python
 install-vs-ext esbenp.prettier-vscode
 install-vs-ext ms-dotnettools.csharp
 install-vs-ext ms-vscode.node-debug2
-install-vs-ext ms-vscode.go
+install-vs-ext golang.go
